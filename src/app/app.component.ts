8de2cd61b96bb20155c4e43e030bf7a452d68f51
Jasper Sommer <jasper.sommer97@gmail.com>
2021-10-06 16:58:28 -0400

    setup for components, service, and models

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mos-dicely-cantina-frontend';
+  title = 'Mos Dicely Cantina';
 }
