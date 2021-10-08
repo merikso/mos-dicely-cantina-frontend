ff84ecb5844c73824cce8a1369a9d0bb726162d7
Jasper Sommer <jasper.sommer97@gmail.com>
2021-10-08 17:20:33 -0400

    working on blackjack component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { BlackjackComponent } from './components/blackjack/blackjack.component';
 import { CardsComponent } from './components/cards/cards.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -5,6 +6,7 @@ import { DeckComponent } from './components/deck/deck.component';
 
 const routes: Routes = [
 
+  {path: 'blackjack', component: BlackjackComponent},
   {path: 'cards', component: CardsComponent},
   {path: '', component: DeckComponent},
   {path: '**', component: DeckComponent}
--- src/app/components/blackjack/blackjack.component.ts
+++ src/app/components/blackjack/blackjack.component.ts
@@ -1,4 +1,7 @@
+import { DeckService } from './../../services/deck.service';
 import { Component, OnInit } from '@angular/core';
+import { Card } from 'src/app/models/card';
+import { CardService } from './../../services/card.service';
 
 @Component({
   selector: 'app-blackjack',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlackjackComponent implements OnInit {
 
-  constructor() { }
+  deck_id = ""
+  remaining = -1;
+  public playerCards: Card[] = [];
+  public dealerCards: Card[] = [];
+  
+  constructor(private cardService: CardService, private deckService: DeckService) { }
+
+  public newDeck() {
+    this.deckService.newDeck()
+      .subscribe(data => { this.deck_id = data.deck_id, this.remaining = data.remaining})
+  }
+
+  public newHands() {
+    this.cardService.draw(this.deck_id, 2)
+      .subscribe(data => { this.dealerCards = data.cards, this.remaining= data.remaining })
+    this.cardService.draw(this.deck_id, 2)
+      .subscribe(data => { this.playerCards = data.cards, this.remaining= data.remaining })
+  }
+
+  public hit() {
+    this.cardService.draw(this.deck_id, 1)
+      .subscribe(data => this.playerCards.push(data.cards[0]))
+  }
 
   ngOnInit(): void {
   }
+
+
 
 }
