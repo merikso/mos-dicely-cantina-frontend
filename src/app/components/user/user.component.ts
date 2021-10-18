383f66d5033bfa0ca07b8f05dc3232797ca26a8a
Matthew <merikso@g.clemson.edu>
2021-10-18 03:10:51 -0400

    balance on profile screen

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { UserArray } from './../../models/user';
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/models/user'
+
+
 
 @Component({
   selector: 'app-user',
@@ -10,9 +15,33 @@ export class UserComponent implements OnInit {
   username = sessionStorage.getItem("username")
   credits = sessionStorage.getItem("chips")
 
-  constructor() { }
+  u_id = 0
+  chips = 0
+
+  constructor(private userService: UserService) { }
+
+  public getUser() {
+    let uarray: UserArray = new UserArray([])
+    let uarr: User[] = []
+    let string = ""
+    
+
+    this.userService.findAllUsers()
+    .subscribe(data => string = JSON.stringify(data))
+    .add(() => { uarr = JSON.parse(string), console.log(uarr), 
+    this.u_id = this.userService.findUserId(sessionStorage.getItem('username')!, uarr)
+    console.log(this.u_id)
+    this.getUserById()
+  })
+  }
+
+  public getUserById() {
+    this.userService.findByUserId(this.u_id)
+    .subscribe(data => {this.u_id = data.id, this.chips = data.chips})
+  }
 
   ngOnInit(): void {
+    this.getUser();
   }
 
 }
