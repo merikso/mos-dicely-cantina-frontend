11ed00069c97d2733b772dca834d43c310124dbc
merikso <57417191+merikso@users.noreply.github.com>
2021-10-17 23:29:24 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   cdUrl: 'https://deckofcardsapi.com/api/deck/',
-  bkndUrlDeployed: 'http://mosdicelybackend-env.eba-cpytmwf2.us-east-2.elasticbeanstalk.com/',
+  bkndUrlDeployed: 'http://Backendmosdicelyfinal-env.eba-p3vc22w3.us-east-2.elasticbeanstalk.com',
   bkndUrlLocal: 'localhost:5000/',
   production: false,
   localUrl: `http://localhost:5000`
 };
 
 export const localUrl = `http://localhost:5000`
-export const awsUrl = ``
+export const awsUrl = `http://Backendmosdicelyfinal-env.eba-p3vc22w3.us-east-2.elasticbeanstalk.com`
